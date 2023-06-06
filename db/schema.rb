03fa3e8cd9b92b334ec46ca9a8ce1b4c7057270d
brhanuhailu <hailubrhanu29@gmail.com>
2023-06-06 13:35:11 -0700

    fix relationship

--- db/schema.rb
+++ db/schema.rb
@@ -20,6 +20,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_06_181558) do
     t.string "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_comments_on_author_id"
+    t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
   create_table "likes", force: :cascade do |t|
@@ -27,6 +29,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_06_181558) do
     t.integer "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_likes_on_author_id"
+    t.index ["post_id"], name: "index_likes_on_post_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -37,6 +41,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_06_181558) do
     t.integer "likes_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -48,4 +53,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_06_181558) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "users", column: "author_id"
+  add_foreign_key "likes", "users", column: "author_id"
+  add_foreign_key "posts", "users", column: "author_id"
+  add_foreign_key "comments", "posts"
+  add_foreign_key "likes", "posts"
 end
