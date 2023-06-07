a6c9dee7fd6dbd19f0b95d309b4087752cfc844e
brhanuhailu <hailubrhanu29@gmail.com>
2023-06-07 09:35:30 -0700

    fixed linter rubocop

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,10 +1,10 @@
 class Comment < ApplicationRecord
-    belongs_to :author, foreign_key: :author_id, class_name: :'User'
-    belongs_to :post, foreign_key: :post_id, class_name: :'Post'
+  belongs_to :author, foreign_key: :author_id, class_name: :User
+  belongs_to :post, foreign_key: :post_id, class_name: :Post
 
-    after_create :update_comment_counter
+  after_create :update_comment_counter
 
-    def update_comment_counter
-        post.increment!(:comments_counter)
-    end
+  def update_comment_counter
+    post.increment!(:comments_counter)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,15 +1,15 @@
 class Post < ApplicationRecord
-    belongs_to :author, foreign_key: :author_id, class_name: :'User'
-    has_many :comments, foreign_key: :post_id, class_name: :'Comment'
-    has_many :likes, foreign_key: :post_id, class_name: :'Like'
+  belongs_to :author, foreign_key: :author_id, class_name: :User
+  has_many :comments, foreign_key: :post_id, class_name: :Comment
+  has_many :likes, foreign_key: :post_id, class_name: :Like
 
-    after_create :update_post_counter
+  after_create :update_post_counter
 
-    def update_post_counter
-        author.increment!(:posts_counter)
-    end
+  def update_post_counter
+    author.increment!(:posts_counter)
+  end
 
-    def most_recent_comments
-        comments.order(created_at: :desc).limit(5)
-    end
+  def most_recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end
