ba14ba14a428c0855f0f6c8a30455e373286ec27
brhanuhailu <hailubrhanu29@gmail.com>
2023-06-14 09:54:28 -0700

    post

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,8 +6,6 @@ class Post < ApplicationRecord
   after_create :update_post_counter
 
   validates :title, presence: true, length: { maximum: 250 }
-  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def update_post_counter
     author.increment!(:posts_counter)
