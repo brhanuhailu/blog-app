319bf80b5c57b132e8f2b40b454dccf3bc69a875
brhanuhailu <hailubrhanu29@gmail.com>
2023-06-14 10:54:23 -0700

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,10 @@ Rails.application.routes.draw do
   # root "articles#index"
   root "users#index"
 
-
-  get 'posts/new', to: 'posts#new'
-  post 'posts', to: 'posts#create'
-  post 'likes/:post_id', to: 'likes#create'
-  post 'comments/:post_id', to: 'comments#create'
+  resources :posts, only: [:new, :create] do
+    resources :likes, only: [:create]
+    resources :comments, only: [:create]
+  end
 
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show]
