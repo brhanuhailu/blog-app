119b0112a5888e5467d486ae2e2ef55023913aff
brhanuhailu <hailubrhanu29@gmail.com>
2023-06-21 06:30:46 -0700

    users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,18 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
   def index
     @users = User.all
     @current_user = current_user
   end
 
   def show
-    @user = User.find(params[:id])
+    if params[:id] == 'sign_out'
+      sign_out(current_user)
+      redirect_to root_path
+    else
+      @user = User.find(params[:id])
+      @posts = @user.posts
+      @current_user = current_user
+    end
   end
 end
