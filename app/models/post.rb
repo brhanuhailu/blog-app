95834bb341827daa1e9741a04018b2670fd7a9dc
brhanuhailu <hailubrhanu29@gmail.com>
2023-06-21 06:32:49 -0700

    models post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :author, foreign_key: :author_id, class_name: :User
-  has_many :comments, foreign_key: :post_id, class_name: :Comment
-  has_many :likes, foreign_key: :post_id, class_name: :Like
+  has_many :comments, foreign_key: :post_id, class_name: :Comment, dependent: :destroy
+  has_many :likes, foreign_key: :post_id, class_name: :Like, dependent: :destroy
 
   after_create :update_post_counter
 
