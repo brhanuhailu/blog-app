5a8b5b504c137cfd1dc4e6835d31e1c6ccc35d9f
brhanuhailu <hailubrhanu29@gmail.com>
2023-06-22 05:47:00 -0700

    posts controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,10 @@
 class PostsController < ApplicationController
+  load_and_authorize_resource
+
   def index
     @user = User.find_by(id: params[:user_id])
     @posts = @user.posts.includes(:comments).paginate(page: params[:page], per_page: 2)
+    @current_user = current_user
   end
 
   def show
@@ -16,11 +19,26 @@ class PostsController < ApplicationController
   end
 
   def create
-    @user = User.find(params[:user_id])
+    @user = current_user
     @post = @user.posts.new(post_params)
     @post.likes_counter = 0
     @post.comments_counter = 0
+    if @post.save
+      redirect_to user_posts_path(@user.id)
+    else
+      render :new
+    end
+  end
 
+  def destroy
+    @user = User.find(params[:user_id])
+    @post = @user.posts.find(params[:id])
+    authorize! :destroy, @post
+    if @post.destroy
+      flash[:notice] = 'Post deleted successfully'
+    else
+      flash[:alert] = 'Something went wrong'
+    end
     redirect_to user_posts_path
   end
 
