4c778bc64c462eceec43a067f3c8dba16e0041dd
brhanuhailu <hailubrhanu29@gmail.com>
2023-06-23 09:29:59 -0700

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,14 @@ Rails.application.routes.draw do
       resources :likes, only: [:new, :create]
     end
   end
+  namespace :api do
+    namespace :v1 do
+      post 'login', to: 'login#index'
+      resources :users, only: [:index] do
+        resources :posts, only: [:index] do
+          resources :comments, only: [:index, :create]
+        end
+      end
+    end
+  end
 end
